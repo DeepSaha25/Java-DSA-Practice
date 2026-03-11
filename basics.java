fd34ce27e91e84ac3110dc319822086e52153c92
DeepSaha25 <ideepsaha25@gmail.com>
2026-03-11 19:02:32 +0530

    DAY 1

---
+++ basics.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class basics{
+    public static void main(String args[]){
+    // System.out.print("Hello World\n");
+    // System.out.println("****");
+     Scanner scn =new Scanner(System.in);
+    //  String input = scn.next();
+    //  System.out.println(input);
+
+    //  String name = scn.nextLine();
+    //  System.out.println(name);
+   System.out.print("Enter First number");
+    int a =scn.nextInt();
+    System.out.print("Enter Second number");
+    int b = scn.nextInt();
+    int sum = a+b;
+    System.out.println(sum);
+
+
+    }
+}
---
+++ variables.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class variables {
+    public static void main(String args[]){
+       /* Question 1 : In a program, input 3 numbers : A, B and C. You have to output the average of
+        these 3 numbers.*/
+        // Scanner s = new Scanner(System.in);
+        // System.out.print("Enter first number ");
+        // float A = s.nextFloat();
+        // System.out.print("Enter second number ");
+        // float B = s.nextFloat();
+        // System.out.print("Enter third number ");
+        // float C = s.nextFloat();
+
+        // float avg = (A+B+C)/3;
+        // System.out.print(avg);
+
+        // --------------------------------------------------------
+      
+        /*Question 2: In a program, input the side of a square. You have to output the area of the
+        square*/ 
+    }
+    
+}
