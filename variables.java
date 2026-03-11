411fa8aadbd3992e222b5a44e143a622827a0541
DeepSaha25 <ideepsaha25@gmail.com>
2026-03-11 19:25:00 +0530

    q4

--- variables.java
+++ variables.java
@@ -18,12 +18,34 @@ public class variables {
       
         /*Question 2: In a program, input the side of a square. You have to output the area of the
         square*/ 
-       Scanner s = new Scanner(System.in);
+       
         // System.out.print("Enter side of a square ");
         // float side = s.nextFloat();
         // float area = side*side;
         // System.out.println(area);
-       
+        // ------------------------------------------------------------
+
+        /*Question 3: Enter cost of 3 items from the user (using float data type) - a pencil, a pen and
+        an eraser. You have to output the total cost of the items back to the user as their bill. */
+
+        // System.out.print("Enter the cost of pencil ");
+        // float p = s.nextFloat();
+        // System.out.print("Enter the cost of pen ");
+        // float pen =s.nextFloat();
+        // System.out.print("Enter the cost of eraser ");
+        // float e =s.nextFloat();
+        //  float total = p+pen+e;
+        //  System.out.print(total);
+// ---------------------------------------------------------------------
+
+        Scanner s = new Scanner(System.in);
+        // int $=24;
+        // System.out.print($);
+
+
+
+
+
     }
     
 }
