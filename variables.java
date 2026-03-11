d35f4529e751058b456094e047c3dc00c08638d6
DeepSaha25 <ideepsaha25@gmail.com>
2026-03-11 19:16:07 +0530

    2

--- variables.java
+++ variables.java
@@ -3,7 +3,7 @@ public class variables {
     public static void main(String args[]){
        /* Question 1 : In a program, input 3 numbers : A, B and C. You have to output the average of
         these 3 numbers.*/
-        // Scanner s = new Scanner(System.in);
+        
         // System.out.print("Enter first number ");
         // float A = s.nextFloat();
         // System.out.print("Enter second number ");
@@ -18,6 +18,12 @@ public class variables {
       
         /*Question 2: In a program, input the side of a square. You have to output the area of the
         square*/ 
+       Scanner s = new Scanner(System.in);
+        // System.out.print("Enter side of a square ");
+        // float side = s.nextFloat();
+        // float area = side*side;
+        // System.out.println(area);
+       
     }
     
 }
