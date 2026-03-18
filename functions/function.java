9fb704991ecd1b97d560b433fe846d0bd0d238ab
DeepSaha25 <ideepsaha25@gmail.com>
2026-03-18 10:29:10 +0530

    Add Java functions for sum and multiplication; include settings for debugging

---
+++ functions/function.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class function {
+
+    public static void printSum(int a,int b){
+        int sum=a+b;
+        System.out.printf("the sum is %d\n", sum);
+    }
+
+    public static void main(String args[]){
+    printSum(5,12);
+    }
+}
---
+++ functions/multiply.java
@@ -0,0 +1,15 @@
+public class multiply {
+    public static int mul(int a , int b){
+        int mul=a*b;
+        return mul;
+    }
+
+    public static void main(String args[]){
+        int  x=5;
+        int y=10;
+        int mul=mul(x,y);
+        System.out.println(mul);
+ 
+    }
+
+}
