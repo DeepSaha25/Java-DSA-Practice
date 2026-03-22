4bb25ad84911b51f6de3c6c5bf58753284fa5ba3
Deep Saha <ideepsaha25@gmail.com>
2026-03-22 23:05:35 +0530

    pattern.java to implement various pattern

--- pattern.java
+++ pattern.java
@@ -1,24 +1,127 @@
 import java.util.*;
 public class pattern {
-    public static void main(String args[]){
-    Scanner s = new Scanner(System.in);
-    int n =s.nextInt();
-
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=i;j++){
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
-    // }
-    
-    char ch='A';
+    public static void invertedHalfPyd(int n){
     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+        }
         for(int j=1;j<=i;j++){
-            System.out.print(ch);
-            ch++;
+            System.out.print("*");
         }
         System.out.println();
     }
 
     }
+
+    public static void hollowRec(int totRows, int totCols) {
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void alphabetPyramid(int n) {
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void butterflyPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+           for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void rhombusPattern(int n){
+        for(int i= 1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollowRhombus(int n){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+             System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1||i==n||j==1||j==n){
+                System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamondPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+//   alphabetPyramid(5);
+//   invertedHalfPyd(5);
+// butterflyPattern(4);
+// rhombusPattern(5);
+// hollowRhombus(5);
+diamondPattern(4);
+
+}
 }
